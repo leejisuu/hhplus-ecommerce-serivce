8a4c035e527f0cd17efff01e53e662a06b23ca14
leejisuu <dlwltn966@gmail.com>
2025-01-06 13:47:36 +0900

    [Refector]쿠폰 DTO 변경, 엔드포인트 변경
    * CouponRequest를 CouponIssueRequest로 변경
    * CouponResponse에 변수 추가
    * 엔드포인트 /coupon에서 /api/v1/coupons로 변경

--- src/main/java/kr/hhplus/be/server/interfaces/api/coupon/CouponController.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/coupon/CouponController.java
@@ -2,30 +2,33 @@ package kr.hhplus.be.server.interfaces.api.coupon;
 
 import io.swagger.v3.oas.annotations.Operation;
 import kr.hhplus.be.server.interfaces.api.common.ApiResponse;
-import kr.hhplus.be.server.interfaces.api.coupon.dto.CouponRequest;
+import kr.hhplus.be.server.interfaces.api.coupon.dto.CouponIssueRequest;
 import kr.hhplus.be.server.interfaces.api.coupon.dto.CouponResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("coupon")
+@RequestMapping("/api/v1/coupons")
 public class CouponController {
 
-    @Operation(summary = "Issue coupon", description = "사용자가 쿠폰을 발급받는다.")
+    @Operation(summary = "선착순 쿠폰 발급 API", description = "사용자가 쿠폰을 발급받는다.")
     @PostMapping("issue")
-    public ApiResponse<CouponResponse> issueCoupon(@RequestBody CouponRequest couponRequest) {
+    public ApiResponse<CouponResponse> issueCoupon(@RequestBody CouponIssueRequest couponIssueRequest) {
         CouponResponse response = CouponResponse.builder()
                 .id(1L)
+                .couponId(1L)
                 .name("선착순 쿠폰")
                 .discountType("PERCENTAGE")
                 .discountAmt(10)
-                .validStartAt(LocalDate.of(2025, 1, 1))
-                .validEndAt(LocalDate.of(2025, 2, 1))
-                .status("NOT USED")
+                .issuedAt(LocalDateTime.of(2025, 1, 1, 11, 0, 0))
+                .validStartAt(LocalDateTime.of(2025, 1, 1, 11, 0, 0))
+                .validEndAt(LocalDateTime.of(2025, 2, 1, 11, 0, 0))
+                .usedAt(null)
+                .status("UNUSED")
                 .build();
 
         return ApiResponse.ok(response);
