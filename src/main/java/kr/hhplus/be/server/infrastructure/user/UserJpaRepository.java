f1ac3e007e79de2e2d543ad91d523ae122fc3399
leejisuu <dlwltn966@gmail.com>
2025-01-22 16:01:23 +0900

    [Refector] 유저 조회 메서드명 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/user/UserJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/user/UserJpaRepository.java
@@ -1,16 +1,10 @@
 package kr.hhplus.be.server.infrastructure.user;
 
 import kr.hhplus.be.server.domain.user.entity.User;
-import kr.hhplus.be.server.domain.support.exception.CustomException;
-import kr.hhplus.be.server.domain.support.exception.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
-    default User findByUserIdOrThrow(Long userId) {
-        return findById(userId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-    }
-
-    Optional<User> findById(Long userId);
+    Optional<User> findById(Long id);
 }
