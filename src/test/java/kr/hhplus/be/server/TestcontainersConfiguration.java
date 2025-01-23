c7ad5e6b426defdda47af289d8be57c80d198e5d
leejisuu <dlwltn966@gmail.com>
2025-01-23 09:39:27 +0900

    [Chore] 테스트 컨테이너에 레디스 실행하기 위한 설정

--- src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server;
 
+import com.redis.testcontainers.RedisContainer;
 import jakarta.annotation.PreDestroy;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -8,9 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+@Configuration
 class TestcontainersConfiguration {
 
 	public static final MySQLContainer<?> MYSQL_CONTAINER;
+	public static final RedisContainer REDIS_CONTAINER;
 
 	static {
 		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
@@ -22,12 +25,23 @@ class TestcontainersConfiguration {
 		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
 		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
 		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+
+		REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis"))
+				.withExposedPorts(6379);
+		REDIS_CONTAINER.start();
+
+		// Redis 연결 정보를 Spring 환경 변수에 추가
+		System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+		System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
 	}
 
 	@PreDestroy
 	public void preDestroy() {
 		if (MYSQL_CONTAINER.isRunning()) {
 			MYSQL_CONTAINER.stop();
+		}
+		if (REDIS_CONTAINER.isRunning()) {
+			REDIS_CONTAINER.stop();
 		}
 	}
 }
--- src/test/java/kr/hhplus/be/server/support/IntegrationTestSupport.java
+++ src/test/java/kr/hhplus/be/server/support/IntegrationTestSupport.java
@@ -1,14 +1,15 @@
 package kr.hhplus.be.server.support;
 
-import kr.hhplus.be.server.config.MySQLTestContainerConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Testcontainers
 @SpringBootTest
-public abstract class IntegrationTestSupport implements MySQLTestContainerConfig {
+public abstract class IntegrationTestSupport {
 
     @Autowired
     private DatabaseCleanUp databaseCleanUp;
