9aef85d9784f95331eba44d69dc6e38a946eae8f
leejisuu <dlwltn966@gmail.com>
2025-01-24 05:24:30 +0900

    [Chore] Redisson 분산락 설정 파일
    * Redisson 의존성 주입
    * 락 획득 -> 트랙잭션 시작의 순서를 지키기 위해 AOP 적용

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/redisson/RedissonLock.java
@@ -0,0 +1,29 @@
+package kr.hhplus.be.server.infrastructure.redisson;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RedissonLock {
+
+    /**
+     * 락의 이름
+     */
+    String key();
+
+    /**
+     * 락의 시간 단위
+     */
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    /**
+     * 락 획득을 위해 waitTime 만큼 대기한다
+     */
+    long waitTime() default 5000L;
+
+    /**
+     * 락을 획득한 이후 leaseTime 이 지나면 락을 해제한다
+     */
+    long leaseTime() default 3000L;
+}
