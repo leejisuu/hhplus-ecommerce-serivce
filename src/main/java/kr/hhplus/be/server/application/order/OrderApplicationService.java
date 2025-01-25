cb6af142faa3d11a19543f1472996d1203a59196
leejisuu <dlwltn966@gmail.com>
2025-01-26 01:53:07 +0900

    [Refector]상품 재고 차감 dto 리팩토링
    * OrderCriteria에서 StockCommand, OrderCommand 데이터 세팅

--- src/main/java/kr/hhplus/be/server/application/order/OrderApplicationService.java
+++ src/main/java/kr/hhplus/be/server/application/order/OrderApplicationService.java
@@ -4,19 +4,15 @@ import kr.hhplus.be.server.application.order.dto.criteria.OrderCriteria;
 import kr.hhplus.be.server.application.order.dto.result.OrderResult;
 import kr.hhplus.be.server.infrastructure.redisson.RedissonLock;
 import kr.hhplus.be.server.domain.dataplatform.DataPlatformClient;
-import kr.hhplus.be.server.domain.order.dto.command.OrderCommand;
 import kr.hhplus.be.server.domain.order.service.OrderService;
 import kr.hhplus.be.server.domain.order.dto.info.OrderInfo;
 import kr.hhplus.be.server.domain.product.service.ProductService;
 import kr.hhplus.be.server.domain.product.dto.ProductInfo;
 import kr.hhplus.be.server.domain.product.service.ProductStockService;
-import kr.hhplus.be.server.domain.support.exception.CustomException;
-import kr.hhplus.be.server.domain.support.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -28,30 +24,12 @@ public class OrderApplicationService {
 
     @RedissonLock(key ="'Order:userId:' + #userId")
     public OrderResult.Order order(OrderCriteria.Order criteria) {
-        List<Long> productIds = criteria.details().stream().map(OrderCriteria.OrderDetail::productId).collect(Collectors.toList());
-        List<ProductInfo.ProductDto> products = productService.getProducts(productIds);
-        OrderCommand.Order orderCommand = convertToOrderCommand(criteria, products);
-        for(OrderCommand.OrderDetail detail : orderCommand.details()) {
-            productStockService.deductQuantity(detail.productId(), detail.quantity());
-        }
-        OrderInfo.OrderDto order = orderService.order(orderCommand);
+        List<ProductInfo.ProductDto> products = productService.getProducts(criteria.getProductIds());
+        productStockService.deductQuantity(criteria.toStockCommand());
+        OrderInfo.OrderDto order = orderService.order(criteria.toCommand(products));
 
         dataPlatformClient.sendData(order);
 
         return OrderResult.Order.of(order);
-    }
-
-    private OrderCommand.Order convertToOrderCommand(OrderCriteria.Order order, List<ProductInfo.ProductDto> products) {
-        List<OrderCommand.OrderDetail> commandOrderDetails = order.details().stream()
-                .map(orderDetail -> {
-                    ProductInfo.ProductDto product = products.stream()
-                            .filter(p -> p.id().equals(orderDetail.productId()))
-                            .findFirst()
-                            .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
-                    return new OrderCommand.OrderDetail(orderDetail.productId(), orderDetail.quantity(), product.price());
-                })
-                .collect(Collectors.toList());
-
-        return new OrderCommand.Order(order.userId(), commandOrderDetails);
     }
 }
--- src/main/java/kr/hhplus/be/server/application/order/dto/criteria/OrderCriteria.java
+++ src/main/java/kr/hhplus/be/server/application/order/dto/criteria/OrderCriteria.java
@@ -1,8 +1,14 @@
 package kr.hhplus.be.server.application.order.dto.criteria;
 
+import kr.hhplus.be.server.domain.order.dto.command.OrderCommand;
+import kr.hhplus.be.server.domain.product.dto.ProductInfo;
+import kr.hhplus.be.server.domain.product.dto.StockCommand;
+import kr.hhplus.be.server.domain.support.exception.CustomException;
+import kr.hhplus.be.server.domain.support.exception.ErrorCode;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderCriteria {
     public record Order(
@@ -12,6 +18,37 @@ public class OrderCriteria {
 
         @Builder
         public Order {}
+
+        public StockCommand.OrderDetails toStockCommand() {
+            List<StockCommand.OrderDetail> orderDetils = details.stream()
+                    .map(orderDetail -> {
+                        return StockCommand.OrderDetail.builder()
+                                .productId(orderDetail.productId)
+                                .quantity(orderDetail.quantity)
+                                .build();
+                    })
+                    .collect(Collectors.toList());
+
+            return new StockCommand.OrderDetails(orderDetils);
+        }
+
+        public OrderCommand.Order toCommand(List<ProductInfo.ProductDto> products) {
+            List<OrderCommand.OrderDetail> commandOrderDetails = this.details().stream()
+                    .map(orderDetail -> {
+                        ProductInfo.ProductDto product = products.stream()
+                                .filter(p -> p.id().equals(orderDetail.productId()))
+                                .findFirst()
+                                .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+                        return new OrderCommand.OrderDetail(orderDetail.productId(), orderDetail.quantity(), product.price());
+                    })
+                    .collect(Collectors.toList());
+
+            return new OrderCommand.Order(this.userId(), commandOrderDetails);
+        }
+
+        public List<Long> getProductIds() {
+            return this.details().stream().map(OrderCriteria.OrderDetail::productId).collect(Collectors.toList());
+        }
     }
 
     public record OrderDetail(
