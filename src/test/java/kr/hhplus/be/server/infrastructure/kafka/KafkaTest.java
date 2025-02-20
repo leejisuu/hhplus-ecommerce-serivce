2859e5d0a20ca3337e99ef06acfa72c37ca93b38
leejisuu <dlwltn966@gmail.com>
2025-02-21 05:53:36 +0900

    [Test] 카프카 연결 테스트 코드 작성

--- src/test/java/kr/hhplus/be/server/infrastructure/kafka/KafkaTest.java
+++ src/test/java/kr/hhplus/be/server/infrastructure/kafka/KafkaTest.java
@@ -1,43 +1,36 @@
 package kr.hhplus.be.server.infrastructure.kafka;
-import kr.hhplus.be.server.support.IntegrationTestSupport;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
-@Testcontainers
 @SpringBootTest
 class KafkaTest {
+    private static final String TOPIC = "test";
+    private static final AtomicInteger counter = new AtomicInteger(0);
 
     @Autowired
-    private TestProducer testProducer;
-    @Autowired
-    private TestConsumer testConsumer;
-    private final String TOPIC = "test-topic";
+    private KafkaTemplate<String, Object> producer;
+
+    @KafkaListener(topics = TOPIC, groupId = "test_group")
+    public void listen(String message) {
+        counter.incrementAndGet();
+    }
 
     @Test
-    void Kafka_연동_테스트() {
-        // given
-        List<String> messages = new ArrayList<>();
-        int cnt = 5;
-        for (int i = 0; i < cnt; i++) {
-            String message = "Kafka 연동 테스트 " + i;
-            testProducer.send(TOPIC, message);
-            messages.add(message);
-        }
-
-        // when // then
-        await().pollDelay(2, TimeUnit.SECONDS)
-                .atMost(10, TimeUnit.SECONDS)
-                .untilAsserted(() -> assertThat(testConsumer.getMessages())
-                        .hasSize(cnt));
+    void kafkaTest() {
+        producer.send(TOPIC, "Kafka Test");
+        await()
+                .pollInterval(Duration.ofMillis(300))
+                .atMost(Duration.ofSeconds(2))
+                .untilAsserted(() -> assertThat(counter.get()).isEqualTo(1L));
     }
 }
